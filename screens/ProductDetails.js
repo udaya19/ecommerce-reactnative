6541adc50f7501b326a2f960dbbd7ae876522e94
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2023-01-17 09:26:43 +0530

    Product details page

--- screens/ProductDetails.js
+++ screens/ProductDetails.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Text,
   StyleSheet,
@@ -9,11 +9,28 @@ import {
   Button,
 } from "react-native";
 
-function ProductDetails(props) {
+import { getProduct } from "../services/ProductService";
+
+function ProductDetails({ route }) {
+  const [product, setProduct] = useState({});
+  const { productId } = route.params;
+  useEffect(() => {
+    setProduct(getProduct(productId));
+  }, []);
   return (
-    <View style={styles.container}>
-      <Text>Product Details</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <View style={styles.imageContainer}>
+          <Image style={styles.image} source={product.image} />
+        </View>
+        <View style={styles.infoContainer}>
+          <Text style={styles.name}>{product.name}</Text>
+          <Text style={styles.price}>${product.price}</Text>
+          <Text style={styles.description}>{product.description}</Text>
+          <Button title="Add to Basket" />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
